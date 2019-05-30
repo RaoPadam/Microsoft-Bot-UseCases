95adbf7062ed9fef1f17c9e40216644a2d0d767c
padamrao <padam.rao@gmail.com>
2019-05-30 14:39:39 +0000

    Poc for  AzureAD and Microsoft Graph Auth

---
+++ AzureGraphAuth/AzureGraphAuthApp/WebApplication1/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using AzureGraphAuthApp.Extensions;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureGraphAuthApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public AuthCodeFlowController Oauth { get; }
+
+        public AuthController(AuthCodeFlowController oauth)
+        {
+            Oauth = oauth;
+        }
+
+        [HttpGet("userconsentredirect")]
+        public async Task<ContentResult> GetUserConsent(string code, string state)
+        {
+            await Oauth.UserConsented(code, state);
+            return Content("Thanks for providing user consent, you may now close this window.");
+        }
+    }
+}
---
+++ AzureGraphAuth/AzureGraphAuthApp/WebApplication1/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AzureGraphAuthApp.Controllers;
+
+namespace AzureGraphAuthApp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json")
+               .AddEnvironmentVariables();
+            this.Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(Configuration);
+            services.AddSingleton(new AuthCodeFlowController(Configuration));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
+
+
+
